a0e7366d5b12de8d3872d9eb54c9d20d87f6dc19
Firat Kucuk <firat.kucuk@doculayer.com>
2021-04-16 09:55:15 +0200

    Added support for private repos

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# repsy-maven-importer
+Maven repo importer for repsy
+
+This small script helps importing an external repository into repsy. Please fill the variables section in the script first.
+
+```python
+# external maven repo ##########################################################
+
+# Fix the URL (Do NOT forget trailing slash)
+maven_repo_link = 'https://MYUSERNAME.bintray.com/repo/'
+maven_repo_username = '' # keep empty string if repo is public
+maven_repo_password = ''
+
+# repsy repo ###################################################################
+
+# Fix the URL (Do NOT forget trailing slash)
+repsy_repo_link = 'https://repo.repsy.io/mvn/MYUSERNAME/MYREPO/'
+repsy_repo_username = 'fixme'
+repsy_repo_password = 'fixme'
+
+################################################################################
+```
+
+then you can start the migration:
+
+```bash
+python3 ./main.py
+```
--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 
 # external maven repo ##########################################################
 
-# Fix the URL
+# Fix the URL (Do NOT forget trailing slash)
 maven_repo_link = 'https://MYUSERNAME.bintray.com/repo/'
-maven_repo_username = ''
+maven_repo_username = '' # keep empty string if repo is public
 maven_repo_password = ''
 
 # repsy repo ###################################################################
 
-# Fix the URL
+# Fix the URL (Do NOT forget trailing slash)
 repsy_repo_link = 'https://repo.repsy.io/mvn/MYUSERNAME/MYREPO/'
 repsy_repo_username = 'fixme'
 repsy_repo_password = 'fixme'
@@ -22,23 +22,29 @@ import base64
 
 anchor_regex = re.compile('<a.*<\/a>')
 anchor_link_regex = re.compile('.*href="(.*)"')
-repsy_headers = {
-    'Authorization': 'Basic ' + base64.b64encode((repsy_repo_username + ':' + repsy_repo_password).encode('ascii')).decode('ascii')
-}
+maven_headers = {}
+
+def basic_auth(username: str, password: str):
+    return base64.b64encode((username + ':' + password).encode('ascii')).decode('ascii')
+
+if maven_repo_username != '':
+    maven_headers = {'Authorization': 'Basic ' + basic_auth(maven_repo_username, maven_repo_password)}
+
+repsy_headers = {'Authorization': 'Basic ' + basic_auth(repsy_repo_username, repsy_repo_password)}
 
 def upload_repsy(path: str):
-    response = requests.get(maven_repo_link + path)
-    upload_response = requests.put(repsy_repo_link + path, data = response.content, headers = repsy_headers)
+    response = requests.get(maven_repo_link + path, headers = maven_headers)
+    requests.put(repsy_repo_link + path, data = response.content, headers = repsy_headers)
 
 
 def walk(path: str):
-    response = requests.get(maven_repo_link + path)
+    response = requests.get(maven_repo_link + path, headers = maven_headers)
 
     for match in anchor_regex.finditer(response.text):
         link_matches = anchor_link_regex.match(match.group())
 
         if len(link_matches.groups()) > 0:
-            child_path = link_matches.group(1)
+            child_path = re.sub("\?.*", "a", link_matches.group(1))
 
             if child_path.endswith('/'):
                 walk(path + child_path)
